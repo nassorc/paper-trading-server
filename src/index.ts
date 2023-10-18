7f9b0209771f9b440cab5af5a6ea37cd749da27e
nassorc <macrossan17@gmail.com>
2023-10-18 00:01:13 -0700

    Add a file to start a server and handle server shutdowns

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import { buildServer } from "./build_server";
+
+const PORT = parseInt(process.env.PORT || "3001");
+
+async function main() {
+  const app = await buildServer();
+
+  app.listen({ port: PORT }, (err) => {
+    if (err) {
+      app.log.error(err);
+      process.exit(1);
+    }
+
+    ["SIGINT", "SIGTERM"].forEach((signal) => {
+      process.on(signal, async () => {
+        await app.close();
+      });
+    });
+  });
+}
+
+main();
