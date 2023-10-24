8f7cb63ac7fe116360965fc04aeffd45338a9bcf
nassorc <macrossan17@gmail.com>
2023-10-24 00:57:15 -0700

    Implement user services
    - logins and registrations
    - querying user's private and public data

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,3 +1,8 @@
+import { Prisma, PrismaClient, User } from "@prisma/client";
+import { Optional } from "types";
+
+type Credentials = Optional<User, "id">;
+
 export interface UserType {
   username: string;
   password: string;
@@ -6,11 +11,89 @@ export interface UserType {
 export interface IUserCollection {}
 
 class UserService {
-  constructor(private collections: IUserCollection) {}
-  loginUser(credentials: UserType) {}
-  registerUser(user: UserType) {}
-  searchUser(query: string) {}
-  getUserProfile(userId: string) {}
+  constructor(private userCollection: Prisma.UserDelegate) {}
+
+  async loginUser(credentials: Credentials): Promise<Credentials> {
+    const user = await this.userCollection.findFirst({
+      where: {
+        username: {
+          equals: credentials.username,
+        },
+        password: {
+          equals: credentials.password,
+        },
+      },
+    });
+    if (!user) throw new Error("UserNotFound");
+    return user;
+  }
+
+  async registerUser(user: Credentials): Promise<User> {
+    const UNIQUE_CONSTRAINT_FAILED = "P2002";
+    const { username, password } = user;
+
+    const res = await this.userCollection.create({
+      data: {
+        username,
+        password,
+        wallet: {
+          create: {},
+        },
+        portfolio: {
+          create: {},
+        },
+      },
+    });
+    return res;
+  }
+
+  async registerUserWithWallet(user: Credentials) {
+    // TODO?: use Wallet Serivce to create the wallet
+    const newUserWithWallet = await this.userCollection.create({
+      data: {
+        ...user,
+        wallet: {
+          create: {
+            funds: 0,
+          },
+        },
+        portfolio: {
+          create: {},
+        },
+      },
+    });
+  }
+  async getUserById(userId: number): Promise<User> {
+    const user = await this.userCollection.findFirst({
+      where: {
+        id: {
+          equals: userId,
+        },
+      },
+      include: {
+        portfolio: {
+          include: {
+            stocks: true,
+          },
+        },
+        wallet: true,
+      },
+    });
+    console.log(user);
+    if (!user) throw new Error("UserNotFound");
+    return user;
+  }
+  async getUserProfile(username: string): Promise<User> {
+    const user = await this.userCollection.findFirst({
+      where: {
+        username: {
+          equals: username,
+        },
+      },
+    });
+    if (!user) throw new Error("UserNotFound");
+    return user;
+  }
 }
 
 export default UserService;
