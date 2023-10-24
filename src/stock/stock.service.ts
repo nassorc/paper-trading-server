d23e101d5cd10f1996e7bda15128553814c04319
nassorc <macrossan17@gmail.com>
2023-10-24 00:59:57 -0700

    Implement stock trading operations
    - purchasing a single stock
    - get current stock quote

--- src/stock/stock.service.ts
+++ src/stock/stock.service.ts
@@ -1,29 +1,61 @@
 import { IStockDataSource } from "types";
-import Wallet, { InsufficientFunds } from "../wallet/wallet.service";
+import WalletService, { InsufficientFunds } from "../wallet/wallet.service";
+import { PrismaClient, Prisma } from "@prisma/client";
 
 class StockService {
-  constructor(private stockCollection: IStockDataSource) {
-    this.stockCollection = stockCollection;
-  }
-  async getStockQuote(symbol: string) {
-    return await this.stockCollection.getStockQuote(symbol);
-  }
-  async purchaseStock(
-    symbol: string,
-    quantity: number,
-    wallet: InstanceType<typeof Wallet>
-  ) {
-    const stockQuote = await this.getStockQuote(symbol);
-    // if wallet has sufficient funds
+  constructor(
+    private stockAPIClient: IStockDataSource,
+    private walletClient: WalletService,
+    private portfolioCollection: Prisma.PortfolioDelegate
+  ) {}
 
-    try {
-      const totalPrice = stockQuote.price * quantity;
-      wallet.subtractAmount(totalPrice);
-    } catch (err) {
-      throw err;
-    }
-    // else don't allow transaction
+  async getStockQuote(symbol: string) {
+    return await this.stockAPIClient.getStockQuote(symbol);
   }
+
+  async purchaseStock(userId: number, symbol: string, quantity: number) {
+    // get current stock quote
+    const stockQuote = await this.getStockQuote(symbol);
+    // get user's wallet
+    const userWallet: any = await this.walletClient.getWalletByUserId(userId);
+    if (!stockQuote) throw new Error("Cannot fetch stock quote");
+    if (!userWallet)
+      throw new Error(
+        "Wallet not found. Either User does not exist or User does not have a wallet"
+      );
+    const total = stockQuote.price * quantity;
+    if (userWallet.funds < total)
+      throw new Error("Insufficient funds. Cannot purchase stock");
+
+    // subtract amount from wallet
+    await this.walletClient.subtractAmount(userId, total);
+    // update user's portfolio to reflect purchased stock
+
+    await this.portfolioCollection.update({
+      where: {
+        investorId: userId,
+      },
+      data: {
+        stocks: {
+          upsert: {
+            where: {
+              stock_portfolio_id: {
+                investorId: userId,
+                symbol: symbol,
+              },
+            },
+            create: { symbol, quantity, investorId: userId },
+            update: {
+              quantity: {
+                increment: quantity,
+              },
+            },
+          },
+        },
+      },
+    });
+  }
+  async sellStockPosition() {}
 }
 
 export default StockService;
