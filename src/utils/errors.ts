a25acbf1c454f997c5f02331870650a5b3637bc7
nassorc <macrossan17@gmail.com>
2023-10-25 01:27:12 -0700

    Implement custom Error classes to handle business logic errors that can communicate with the api's http error handler

--- src/utils/errors.ts
+++ src/utils/errors.ts
@@ -1,7 +1,56 @@
-export default class AppError extends Error {
-  httpCode: number;
-  constructor(message: string, httpCode: number) {
+export const RECORD_NOT_FOUND_CODE = "P2025";
+export class AppError extends Error {
+  statusCode: number;
+  constructor(message: string, statusCode: number) {
     super(message);
-    this.httpCode = httpCode;
+    this.statusCode = statusCode;
   }
 }
+export class NotFound extends AppError {
+  constructor(message: string = "NotFound", statusCode: number = 404) {
+    super(message, statusCode);
+    this.statusCode = statusCode;
+  }
+}
+
+export class UserNotFound extends NotFound {
+  constructor() {
+    super();
+  }
+}
+
+export class DuplicateUsernameError extends AppError {
+  constructor(
+    message: string = "DuplicateUsernameError",
+    statusCode: number = 409
+  ) {
+    super(message, statusCode);
+    this.statusCode = statusCode;
+  }
+}
+
+export class InvalidCredentials extends AppError {
+  statusCode: number;
+  body: any;
+  constructor(
+    message: string = "InvalidCredentials",
+    statusCode: number = 401
+  ) {
+    super(message, statusCode);
+    this.statusCode = statusCode;
+  }
+}
+
+export class InsufficientFunds extends AppError {
+  statusCode: number;
+  body: any;
+  constructor(message: string = "InsufficientFunds", statusCode: number = 400) {
+    super(message, statusCode);
+    this.statusCode = statusCode;
+  }
+}
+// {
+//     "statusCode": 500,
+//     "error": "Internal Server Error",
+//     "message": "UserNotFound"
+// }
