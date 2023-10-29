c83508c7153c54f934d723ed141042aff6f56030
nassorc <macrossan17@gmail.com>
2023-10-29 00:16:34 -0700

    Add an interval function to fetch current stock data from a user's watchlist and push the data to the client

--- src/utils/build_server.ts
+++ src/utils/build_server.ts
@@ -1,5 +1,6 @@
 import Fastify, { FastifyInstance } from "fastify";
 import { dbConnectionPlugin } from "../config/dbConnection";
+import jwt from "jsonwebtoken";
 import "dotenv/config";
 
 // PLUGINS
@@ -7,13 +8,17 @@ import fp from "fastify-plugin";
 import fastifyGracefulShutdown = require("fastify-graceful-shutdown");
 import fastifyJwt = require("@fastify/jwt");
 import { decorateFastifyIntance } from "./decorate_server";
-import { requireUser } from "./require_user";
+import { makeSocketRequireUser, requireUser } from "./require_user";
 
 // ENDPOINT CONTROLLERS
 import StockController from "../stock";
 import UserController from "../user";
 import WalletController from "../wallet";
 import WatchlistController from "../watchlist";
+import fastifyIO from "fastify-socket.io";
+import { Server, Socket } from "socket.io";
+import { NextFn } from "types";
+import { attachWatchlist } from "./attachWatchlist";
 
 const defaultOptions = {
   logger: {
@@ -24,12 +29,46 @@ const defaultOptions = {
   },
 };
 
+declare module "fastify" {
+  interface FastifyInstance {
+    io: Server<{ hello: string }>;
+  }
+}
+
+let timer: any;
+
+async function io(app: FastifyInstance) {
+  app.ready(async () => {
+    app.io.use(makeSocketRequireUser(app)); // jwt token authentication
+    app.io.use(attachWatchlist(app)); // attaches user's watch list to socket instance
+    app.io.on("connection", (socket: any) => {
+      if (!timer) {
+        timer = setInterval(async () => {
+          const updatedWatchlist = await Promise.all(
+            socket.watchlist.map(async (stock: string) => {
+              return await app.stockService.getCachedOrFetchStockQuote(stock);
+            })
+          );
+          socket.emit("stock:minute-update", {
+            id: socket.user.id,
+            data: updatedWatchlist,
+          });
+        }, 2000);
+      }
+      socket.on("disconnect", () => {
+        socket.disconnect();
+      });
+    });
+  });
+}
+
 export function buildServer(serverOpts = defaultOptions): FastifyInstance {
   const jwtSecret = process.env.JWT_SECRET || "secret";
   const fastify = Fastify(serverOpts);
   fastify
-    .register(fastifyGracefulShutdown)
     .register(dbConnectionPlugin)
+    .register(fastifyIO)
+    .register(fastifyGracefulShutdown)
     .register(fp(fastifyJwt), { secret: jwtSecret })
     .register(fp(decorateFastifyIntance))
     .register(fp(requireUser))
@@ -37,6 +76,7 @@ export function buildServer(serverOpts = defaultOptions): FastifyInstance {
     .register(UserController)
     .register(WalletController)
     .register(WatchlistController)
+    .register(io)
     .after(() => {
       fastify.gracefulShutdown(async (signal, next) => {
         if (["SIGINT", "SIGTERM"].includes(signal)) {
