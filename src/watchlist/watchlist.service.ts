a6f98b49b76c31e998f2098cf460513503a13236
nassorc <macrossan17@gmail.com>
2023-10-28 23:23:37 -0700

    Add class to handle watchlist operations

---
+++ src/watchlist/watchlist.service.ts
@@ -0,0 +1,24 @@
+import { Prisma } from "@prisma/client";
+
+export default class WatchlistService {
+  constructor(private watchlistCollection: Prisma.WatchlistDelegate) {}
+  async getUserWatchlist(userId: number) {
+    return await this.watchlistCollection.findFirst({
+      where: {
+        investorId: userId,
+      },
+    });
+  }
+  async addSymbolToWatchlist(userId: number, symbol: string) {
+    await this.watchlistCollection.update({
+      where: {
+        investorId: userId,
+      },
+      data: {
+        symbols: {
+          push: symbol,
+        },
+      },
+    });
+  }
+}
