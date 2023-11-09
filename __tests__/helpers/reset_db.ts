7ebea472fc435ebf5dbb02870036073e26851a11
nassorc <macrossan17@gmail.com>
2023-11-08 22:55:23 -0700

    Update the `reset-db` test helper function to reset the redis instance

--- __tests__/helpers/reset_db.ts
+++ __tests__/helpers/reset_db.ts
@@ -1,6 +1,9 @@
 import prisma from "../../src/libs/prisma";
+import Redis from "../../src/libs/redis";
 
 export default async function () {
+  const redis = new Redis();
+  await redis.flushall();
   await prisma.$transaction(async (prisma) => {
     await prisma.user.deleteMany();
     await prisma.stock.deleteMany();
