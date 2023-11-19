fb91a84ee65a531bdb6add85707b393af1506d94
nassorc <macrossan17@gmail.com>
2023-11-19 16:42:45 -0700

    Fix mobile menu not closing when user navigates through a link

--- frontend/scripts/index.js
+++ frontend/scripts/index.js
@@ -4,22 +4,24 @@ const { initAuthApp } = require("./authApp");
 const { validateToken } = require("./actions");
 const app = document.querySelector("#app");
 
-async function fetchHeader() {
+const fetchHeader = async () => {
   const res = await fetch("./components/header.html");
   const header = await res.text();
   return header;
-}
-function prependHeaderToBody(header) {
+};
+const mountHeader = (header) => {
   const div = document.createElement("div");
   div.innerHTML = header;
   document.body.prepend(div.firstChild);
-}
+  return document.body.firstChild;
+};
 async function getHTML(htmlPath) {
   const res = await fetch(htmlPath);
   const html = await res.text();
   return html;
 }
-async function main() {
+
+const initRouter = async () => {
   const requireUser = async () => {
     const accessToken = localStorage.getItem("accessToken");
     if (!accessToken) {
@@ -31,22 +33,24 @@ async function main() {
     }
   };
   const router = new Router("#app");
-  // ROUTES
-  // PROTECTED ROUTES
   router
     .addRoute("/", await getHTML("./pages/index.html"), [requireUser])
     .addRoute("/auth", await getHTML("./pages/auth.html"), [], () => {
       initAuthApp();
     })
     .init();
+async function main() {
+  initRouter();
+  const headerContent = await fetchHeader();
+  const headerELM = mountHeader(headerContent);
+  headerELM.addEventListener("click", (e) => {
+    const BURGER_ITEM = "burger-item";
+    const elm = e.target;
+    const checkbox = document.querySelector("[data-target='burger-checkbox']");
+    if ("target" in elm.dataset && elm.dataset.target === BURGER_ITEM) {
+      checkbox.checked = false;
+    }
+  });
 
-  const header = await fetchHeader();
-  prependHeaderToBody(header);
 }
 main();
-window.addEventListener("popstate", () => {
-  console.log("popping");
-});
-window.addEventListener("beforeunload", () => {
-  console.log("before reload");
-});
