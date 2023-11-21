df83753779a2597ea4a888609a8dd052a6dc3292
nassorc <macrossan17@gmail.com>
2023-11-21 16:24:30 -0700

    Include UserRepository as an export in the infrastructure's di.ts file

--- src/infrastructure/di.ts
+++ src/infrastructure/di.ts
@@ -2,11 +2,13 @@ import { PrismaClient } from "@prisma/client";
 import { RedisCommander } from "ioredis";
 import StockMarketAPI from "../stock/api";
 
-import StockRepository from "..//stock/stock.repository";
+import StockRepository from "../stock/stock.repository";
+import UserRepository from "../user/user.repository";
 
 export interface Dependencies {
   db: PrismaClient;
   cache: RedisCommander;
   stockMarketAPI: StockMarketAPI;
   stockRepository: StockRepository;
+  userRepository: UserRepository;
 }
