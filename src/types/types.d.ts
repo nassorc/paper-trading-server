27cebc5775f501991d82f67e1c5eb2e03333cac4
nassorc <macrossan17@gmail.com>
2023-11-28 23:32:56 -0700

    Remove Observer and Subject interface

--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -7,16 +7,6 @@ export interface IStockDataSource {
   getStockQuote: (symbol: string) => Promise<StockQuoteType>;
 }
 
-export interface IObserver {
-  update(subject: ISubject, data: any): void;
-}
-
-export interface ISubject {
-  attach(o: IObserver): void;
-  detach(o: IObserver): void;
-  notify(data: any): void;
-}
-
 export type Optional<T, K extends keyof T> = Pick<Partial<T>, K> & Omit<T, K>;
 
 export type NextFn = () => void;
